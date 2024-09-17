5e9c8c0e21d94cf8df4d23432cbe24dda6cb827e
Fahim Adnan <muhammadfahim010@gmail.com>
2024-09-17 21:24:26 +0600

    added all routes and necessary pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/all-appointments/', function(){
+    return view('all_appointments');
+});
+
+Route::get('/book-appointment/', function(){
+    return view('book_appointment');
+});
+
+Route::get('/consultation/', function(){
+    return view('consultation_page');
+});
+
+Route::get('/doctor/login/', function(){
+    return view('doctor_login');
+});
+
+Route::get('/doctor/register/', function(){
+    return view('doctor_register');
+});
+
+Route::get("/prescription-details/", function(){
+    return view('prescription_details');
+});
+
+Route::get('/prescription-write/', function(){
+    return view('prescription-write');
+});
+
+Route::get('/search/', function(){
+    return view('search_doctor');
+});
+
+Route::get('/user/login/', function(){
+    return view('user_login');
+});
+
+Route::get('/user/register/', function(){
+    return view('user_register');
+});
