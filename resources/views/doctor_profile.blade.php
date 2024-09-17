3f3775aab537943b17dc5ae2d13acfd39325807d
ummetalhameem23 <157884536+ummetalhameem23@users.noreply.github.com>
2024-09-17 23:56:29 +0600

    doctor login page and update doctor profile page

--- resources/views/doctor_profile.blade.php
+++ resources/views/doctor_profile.blade.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doctor's Profile</title>
     <link rel="stylesheet" href='/css/doctor_profile_styles.css'>
 </head>
-
 <body>
     <div class="profile-container">
         <div class="profile-header">
@@ -16,25 +14,27 @@
                 <h1>Dr. Abir</h1>
                 <h3>Cardiologist</h3>
                 <p class="experience">Experience: 15 years</p>
+                
             </div>
         </div>
-
+        
         <div class="profile-body">
             <h2>About Me</h2>
             <p>
-                Dr. Abir is a highly skilled cardiologist with over 15 years of experience in treating patients with
-                heart conditions.
-                He is dedicated to providing the best care possible and staying updated with the latest advancements in
-                cardiac medicine.
+                Dr. Abir is a highly skilled cardiologist with over 15 years of experience in treating patients with heart conditions.
+                He is dedicated to providing the best care possible and staying updated with the latest advancements in cardiac medicine.
             </p>
 
             <h2>Contact Information</h2>
             <p><strong>Email:</strong> abir@example.com</p>
             <p><strong>Phone:</strong> +123 456 7890</p>
 
+
+            <h2>Consultation Fees</h2>
+            <p><strong>Fees:</strong> 700 tk only</p>
+
             <button class="appointment-button">Schedule an Appointment</button>
         </div>
     </div>
 </body>
-
 </html>
