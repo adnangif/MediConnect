f5eaaf8085923b037d0672fc7e316db6129783e9
Gayotry Toma <gayotrytoma@gmail.com>
2024-09-21 00:37:47 +0600

    Landing Page created, web.php updated

---
+++ resources/views/landing_page.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MediConnect - Telemedicine</title>
+    <link rel="stylesheet" href='/css/landing_page_styles.css'>
+</head>
+<body>
+    <div class="container">
+        <!-- Header section -->
+        <header>
+            <div class="logo">
+                <img src="logo.png" alt="MediConnect Logo">
+            </div>
+            <div class="buttons">
+                <button class="btn register">Register</button>
+                <button class="btn sign-in">Sign In</button>
+            </div>
+        </header>
+
+        <!-- Site Name and Slogan -->
+        <div class="site-content">
+            <h1 class="site-name">MediConnect</h1>
+            <p class="slogan">Your one-stop telemedicine solution</p>
+
+            <!-- Get Started Button -->
+            <button class="btn get-started">Get Started</button>
+        </div>
+    </div>
+</body>
+</html>
