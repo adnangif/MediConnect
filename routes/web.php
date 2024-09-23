d90bf70edf19323ef85db4f091d844e923eb6c5c
Fahim Adnan <muhammadfahim010@gmail.com>
2024-09-23 10:01:17 +0600

    homepage added

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\doctor_profile;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home_page');
 });
 
 Route::get('/all-appointments/', function () {
