67cf45b47420ce2a1e5e7f8ace43b875962a1133
Miraz <skmirazrahman21@gmail.com>
2024-09-25 01:01:53 +0600

    Database Table Creation completed

---
+++ database/migrations/2024_09_24_153428_create_patients_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patient', function (Blueprint $table) {
+            $table->id('patient_id');
+            $table->string('name');
+            $table->string('grnder');
+            $table->string('age');
+            $table->string('contact');
+            $table->string('address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
---
+++ database/migrations/2024_09_24_185350_create_medicines_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->foreignId('pres_id')
+                ->references('pres_id')
+                ->on('prescription')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+                $table->string('medicine_name')->primary();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicines');
+    }
+};
