2a7fda1ef9ac52e52884a26f5c21c9b791c1afe0
Gayotry Toma <gayotrytoma@gmail.com>
2024-09-25 08:34:20 +0600

    Landing Page updated

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -12,19 +12,21 @@
         <!-- Header section -->
         <header>
             <div class="logo">
-                <img src="logo.png" alt="MediConnect Logo">
+                <img src="/image/logo.png" alt="MediConnect Logo">
             </div>
             <div class="buttons">
                 <button class="btn register">Register</button>
                 <button class="btn sign-in">Sign In</button>
             </div>
         </header>
-
+        <!-- Slogan.2 section
+        <div class="slogan.2">
+            <p class="slogan.2">Find the best doctors anytime, anywhere</p>
+        </div> -->
         <!-- Site Name and Slogan -->
         <div class="site-content">
             <h1 class="site-name">MediConnect</h1>
             <p class="slogan">Your one-stop telemedicine solution</p>
-
             <!-- Get Started Button -->
             <button class="btn get-started">Get Started</button>
         </div>
