216facb2f74f3e314f7f0e31b49c381f00db0e60
Fahim Adnan <muhammadfahim010@gmail.com>
2024-10-12 22:30:12 +0600

    login design fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,24 +11,23 @@
 
 
     <div class="login-container">
-        <div class="flex gap-6">
-            <button id="doctor-btn" class="outline-btn grow active" onclick="showDoctorForm()">Doctor</button>
-            <button id="patient-btn" class="outline-btn grow" onclick="showPatientForm()">Patient</button>
+        <div class="grow flex justify-center mb-8">
+            <a href="/" class="flex items-center w-fit">
+                <img class="main-logo" src={{ asset('image/logo.svg') }} />
+                <span class="side-logo">MediConnect</span>
+            </a>
         </div>
-        @if ($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
 
-        <div id="doctor-form" class="mt-4">
-            <h1 class="text-3xl py-2 font-semibold">Doctor Login</h1>
+        <div class="flex gap-6">
+            <button id="doctor-btn" class="outline-btn grow " onclick="showDoctorForm()">Doctor</button>
+            <button id="user-btn" class="outline-btn grow active" onclick="showuserForm()">user</button>
+        </div>
+
+        <div id="doctor-form"  style="display: none;" class="mt-4">
+            <h1 class="text-3xl py-4 font-semibold">Doctor Login</h1>
             <form method="POST">
                 @csrf
+                <input type="hidden" name="role" value="doctor" />
                 <div class="form-group">
                     <label for="email">Email Address:</label>
                     <input type="email" id="email" name="email" placeholder="Enter your email" required>
@@ -38,6 +37,18 @@
                     <label for="password">Password:</label>
                     <input type="password" id="password" name="password" placeholder="Enter your password" required>
                 </div>
+
+                <div>
+                    @if ($errors->any())
+                        <div class="p-2 my-2 bg-red-50 text-red-800 rounded-lg">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                </div>
                 <button type="submit" class="btn w-full">Login</button>
 
                 <p class="forgot-password">
@@ -46,43 +57,48 @@
             </form>
         </div>
 
-        <div id="patient-form" style="display: none;" class="mt-4">
-            <h1 class="text-3xl py-2 font-semibold">Patient Login</h1>
+        <div id="user-form" class="mt-4">
+            <h1 class="text-3xl py-4 font-semibold">User Login</h1>
             <form method="POST">
                 @csrf
+                <input type="hidden" name="role" value="user" />
+
                 <div class="form-group">
-                    <label for="patient-email">Email Address:</label>
-                    <input type="email" id="patient-email" name="email" placeholder="Enter your email" required>
+                    <label for="user-email">Email Address:</label>
+                    <input type="email" id="user-email" name="email" placeholder="Enter your email" required>
                 </div>
 
                 <div class="form-group">
-                    <label for="patient-password">Password:</label>
-                    <input type="password" id="patient-password" name="password" placeholder="Enter your password"
+                    <label for="user-password">Password:</label>
+                    <input type="password" id="user-password" name="password" placeholder="Enter your password"
                         required>
                 </div>
 
+
                 <button type="submit" class="btn w-full">Login</button>
+
 
                 <p class="forgot-password">
                     <a href="#">Forgot your password?</a>
                 </p>
             </form>
         </div>
+
     </div>
 
     <script>
         function showDoctorForm() {
             document.getElementById("doctor-form").style.display = "block";
-            document.getElementById("patient-form").style.display = "none";
+            document.getElementById("user-form").style.display = "none";
             document.getElementById("doctor-btn").classList.add("active");
-            document.getElementById("patient-btn").classList.remove("active");
+            document.getElementById("user-btn").classList.remove("active");
         }
 
-        function showPatientForm() {
+        function showuserForm() {
             document.getElementById("doctor-form").style.display = "none";
-            document.getElementById("patient-form").style.display = "block";
+            document.getElementById("user-form").style.display = "block";
             document.getElementById("doctor-btn").classList.remove("active");
-            document.getElementById("patient-btn").classList.add("active");
+            document.getElementById("user-btn").classList.add("active");
         }
     </script>
 
