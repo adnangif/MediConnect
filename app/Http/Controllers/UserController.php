d91cc05ad92ef3456e27e03f5e31ac89cfebfb7e
Fahim Adnan <muhammadfahim010@gmail.com>
2024-10-12 14:25:25 +0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Session;
-
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
